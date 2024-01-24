cd84893bdab130876c85257a91aec4d1e29eddac
piyushp-zluri <piyush.p@zluri.com>
2024-01-24 13:33:01 +0530

    Completed upload route and added validations. Backend complete

--- app.js
+++ app.js
@@ -1,58 +1,93 @@
 const express = require('express');
-const { MongoClient, ObjectId, Decimal128 } = require('mongodb');
+const mongoose = require('mongoose');
 const multer = require('multer');
-const csvParser = require('csv-parser');
-const { Readable } = require('stream');
+const axios = require('axios');
+const moment = require('moment');
+const currencyCodes = require('currency-codes');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const uri = 'mongodb+srv://piyush_zluri:piyush%401234@cluster0.fqocxgk.mongodb.net/transProjDB?retryWrites=true&w=majority';
-//password is piyush@1234 but it must be converted to percent encoding. Code for '@' is %40
-const client = new MongoClient(uri);
+mongoose.connect('mongodb+srv://piyush_zluri:piyush%401234@cluster0.fqocxgk.mongodb.net/transProjDB?retryWrites=true&w=majority');
+const db = mongoose.connection;
 
-async function connectToDatabase() {
-    try {
-        await client.connect();
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
-}
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
 
-connectToDatabase();
+const transactionSchema = new mongoose.Schema({
+    Date: Date,
+    Description: String,
+    Amount: mongoose.Types.Decimal128,
+    Currency: String
+}, { versionKey: false });
 
-const db = client.db();
-const collection = db.collection('transactions');
+const Transaction = mongoose.model('Transaction', transactionSchema);
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+function isValidDate(date) {
+    return date < new Date();
+}
+
+function isValidCurrency(currency) {
+    return currencyCodes.codes().includes(currency);
+}
+
+const exchangeRateCache = new Map();
+
+async function getExchangeRatesWithINR(currency) {
+    if (exchangeRateCache.size === 0) {
+        try {
+            const response = await axios.get(`https://v6.exchangerate-api.com/v6/a5c66f15ad9c15fcb2f36abb/latest/INR`);
+            const conversionRates = response.data.conversion_rates;
+            for (const [curr, rate] of Object.entries(conversionRates)) {
+                exchangeRateCache.set(curr, rate);
+            }
+        } catch (error) {
+            console.error('Error fetching all exchange rates:', error.message);
+            return null;
+        }
+    }
+
+    if (exchangeRateCache.has(currency)) {
+        return exchangeRateCache.get(currency);
+    } else {
+        console.error(`Exchange rate for ${currency} not found.`);
+        return null;
+    }
+}
+
+async function convertToINR(currency, amount) {
+    const exchangeRate = await getExchangeRatesWithINR(currency);
+
+    if (exchangeRate !== null) {
+        const amountInINR = parseFloat(amount) * exchangeRate;
+        return amountInINR;
+    } else {
+        console.error(`Cannot convert ${amount} ${currency} to INR. Exchange rate not available.`);
+        return null;
+    }
+}
+
 app.post('/api/transactions', async (req, res) => {
     try {
-        const { body } = req;
-        /*
-        if (!body.date || new Date(body.date) >= new Date()) {
-            return res.status(400).json({ error: 'Invalid date. Date should be in the past.' });
-        }
-        if (!body.currency || !isValidCurrency(body.currency)) {
-            return res.status(400).json({ error: 'Invalid currency.' });
-        }
-        */
-        /*
-        if (!body.amount || isNaN(body.amount) || body.amount <= 0) {
-            return res.status(400).json({ error: 'Invalid amount. Amount should be greater than 0.' });
-        }
-        if (!body.description || body.description.trim() === '') {
-            return res.status(400).json({ error: 'Invalid description. Description should be non-empty.' });
-        }
-        */
-
-        const result = await collection.insertOne(body);
-        res.json({ _id: result.insertedId });
-    } catch (error) {
+        const { rawDate, Description, Amount, Currency } = req.body;
+        const formattedDate = moment(rawDate, ['DD-MM-YYYY', 'MM-DD-YYYY', 'DD.MM.YYYY', 'MM.DD.YYYY', 'DD/MM/YYYY', 'MM/DD/YYYY', 'YYYY-MM-DD', 'YYYY.MM.DD', 'YYYY/MM/DD', 'YYYY-DD-MM', 'YYYY.DD.MM', 'YYYY/DD/MM'], true);
+        const transaction = new Transaction({
+            Date: formattedDate,
+            Description: Description,
+            Amount: mongoose.Types.Decimal128.fromString(Amount.toString()),
+            Currency: Currency,
+        });
+        const result = await transaction.save();
+        res.json({ _id: result._id });
+    }
+    catch (error) {
         console.error('Error creating document:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -60,20 +95,20 @@ app.post('/api/transactions', async (req, res) => {
 
 app.get('/api/transactions', async (req, res) => {
     try {
-        const { page = 1, pageSize = 30, Description, Date, Amount, Currency } = req.query;
+        const { page = 1, pageSize = 30, Description, Date, Amount, Currency, amountInINR } = req.query;
         const skip = (page - 1) * pageSize; //Even though page and pageSize are strings, their numerical meaning is considered when arithmetic operations are performed on them. For ex., '30' is treated as 30. Called type coersion
 
         let filter = {};
         if (Description)
             filter.Description = Description;
         if (Date)
-            filter.Date = Date;
+            filter.Date = moment(Date, ['DD-MM-YYYY', 'MM-DD-YYYY', 'DD.MM.YYYY', 'MM.DD.YYYY', 'DD/MM/YYYY', 'MM/DD/YYYY', 'YYYY-MM-DD', 'YYYY.MM.DD', 'YYYY/MM/DD', 'YYYY-DD-MM', 'YYYY.DD.MM', 'YYYY/DD/MM'], true);
         if (Amount)
-            filter.Amount = parseFloat(Amount);
+            filter.Amount = mongoose.Types.Decimal128.fromString(Amount);
         if (Currency)
             filter.Currency = Currency;
 
-        const documents = await collection.find(filter).skip(skip).limit(Number(pageSize)).toArray();
+        const documents = await Transaction.find(filter).skip(skip).limit(Number(pageSize)).lean().exec();
         res.json(documents);
     } catch (error) {
         console.error('Error retrieving documents:', error);
@@ -84,15 +119,14 @@ app.get('/api/transactions', async (req, res) => {
 app.get('/api/transactions/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        if (ObjectId.isValid(id)) {
-            const document = await collection.findOne({ _id: new ObjectId(id) });
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            const document = await Transaction.findById(id).lean().exec();
             if (document) {
                 res.json(document);
             } else {
                 res.status(404).json({ error: 'Document not found' });
             }
-        }
-        else {
+        } else {
             res.status(404).json({ error: 'Invalid ObjectId' });
         }
     } catch (error) {
@@ -104,15 +138,14 @@ app.get('/api/transactions/:id', async (req, res) => {
 app.delete('/api/transactions/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        if (ObjectId.isValid(id)) {
-            const result = await collection.deleteOne({ _id: new ObjectId(id) });
-            if (result.deletedCount > 0) {
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            const result = await Transaction.findByIdAndDelete(id);
+            if (result) {
                 res.json({ message: 'Document deleted successfully' });
             } else {
                 res.status(404).json({ error: 'Document not found' });
             }
-        }
-        else {
+        } else {
             res.status(404).json({ error: 'Invalid ObjectId' });
         }
     } catch (error) {
@@ -125,13 +158,13 @@ app.put('/api/transactions/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const { body } = req;
-        if (ObjectId.isValid(id)) {
-            if (req.body.Amount !== undefined) {
-                req.body.Amount = Decimal128.fromString(req.body.Amount.toString());
-            }
 
-            const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: body });
-            if (result.modifiedCount > 0) {
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            if (body.Date) {
+                body.Date = moment(body.Date, ['DD-MM-YYYY', 'MM-DD-YYYY', 'DD.MM.YYYY', 'MM.DD.YYYY', 'DD/MM/YYYY', 'MM/DD/YYYY', 'YYYY-MM-DD', 'YYYY.MM.DD', 'YYYY/MM/DD', 'YYYY-DD-MM', 'YYYY.DD.MM', 'YYYY/DD/MM'], true);
+            }
+            const result = await Transaction.findByIdAndUpdate(id, { $set: body });
+            if (result) {
                 res.json({ message: 'Document updated successfully' });
             } else {
                 res.status(404).json({ error: 'Document not found' });
@@ -151,49 +184,54 @@ app.post('/api/transactions/uploadCSV', upload.single('csvFile'), async (req, re
             return res.status(400).json({ error: 'No CSV file uploaded.' });
         }
 
-        const parsedData = [];
-        const fileBuffer = req.file.buffer;
+        const transactions = [];
+        const invalidTransactions = [];
 
-        const bufferStream = new Readable();
-        bufferStream.push(fileBuffer);
-        bufferStream.push(null);
-
-        bufferStream
-            .pipe(csvParser())
-            .on('headers', (headerArray) => {
-                headers = headerArray;
-            })
-            .on('data', (row) => {
-                const rowData = {};
-                headers.forEach((header, index) => {
-                    rowData[header] = row[index];
-                });
-                parsedData.push(rowData);
-            })
-            .on('end', async () => {
-                const result = await collection.insertMany(parsedData);
-                res.json({ message: 'CSV data uploaded successfully', insertedIds: result.insertedIds });
+        req.file.buffer
+            .toString('utf8')
+            .split('\n')
+            .slice(1)
+            .forEach((line) => {
+                const [rawDate, Description, Amount, Currency] = line.trim().split(',');
+                const formattedDate = moment(rawDate, ['DD-MM-YYYY', 'MM-DD-YYYY', 'DD.MM.YYYY', 'MM.DD.YYYY', 'DD/MM/YYYY', 'MM/DD/YYYY', 'YYYY-MM-DD', 'YYYY.MM.DD', 'YYYY/MM/DD', 'YYYY-DD-MM', 'YYYY.DD.MM', 'YYYY/DD/MM'], true);
+                if (
+                    isValidDate(formattedDate) &&
+                    Description.trim() !== '' &&
+                    !isNaN(parseFloat(Amount)) && parseFloat(Amount) > 0 &&
+                    isValidCurrency(Currency)
+                ) {
+                    transactions.push({
+                        Date: formattedDate,
+                        Description,
+                        Amount: Amount !== undefined ? mongoose.Types.Decimal128.fromString(Amount.toString()) : undefined,
+                        Currency
+                    });
+                } else {
+                    invalidTransactions.push({
+                        Date: rawDate,
+                        Description,
+                        Amount,
+                        Currency
+                    });
+                }
             });
+
+        for (const transaction of transactions) {
+            const parsedData = new Transaction(transaction);
+            await parsedData.save();
+        }
+
+        if (invalidTransactions.length > 0) {
+            res.status(400).json({
+                invalidTransactions
+            });
+        } else {
+            res.status(200).json({ message: 'All transactions successfully inserted into MongoDB' });
+        }
     } catch (error) {
         console.error('Error uploading CSV file:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-    /*
-    if (!body.date || new Date(body.date) >= new Date()) {
-        return res.status(400).json({ error: 'Invalid date. Date should be in the past.' });
-    }
-    if (!body.currency || !isValidCurrency(body.currency)) {
-        return res.status(400).json({ error: 'Invalid currency.' });
-    }
-    */
-    /*
-    if (!body.amount || isNaN(body.amount) || body.amount <= 0) {
-        return res.status(400).json({ error: 'Invalid amount. Amount should be greater than 0.' });
-    }
-    if (!body.description || body.description.trim() === '') {
-        return res.status(400).json({ error: 'Invalid description. Description should be non-empty.' });
-    }
-    */
 });
 
 app.listen(port, () => {
--- rough.js
+++
@@ -1,2 +0,0 @@
-let now = new Date("2020-05-21");
-console.log(now);
